d37914d9b676677ec71be1b6ec878333097ada4b
ivngmz2018 <ivngmz2018@gmail.com>
2022-03-06 07:32:38 +0000

    Modifico inventario

--- Ansible/shared_files/sshConnections.sh
+++ Ansible/shared_files/sshConnections.sh
@@ -3,6 +3,6 @@
 workdir=`pwd`
 
 masterip=`grep 'masterIP=' $(echo $workdir"/inventory") | cut -d '=' -f2`
-workerip=`grep 'workerIP=' $(echo $workdir"/inventory") | cut -d '=' -f2`
+workerip=`grep 'worker1IP=' $(echo $workdir"/inventory") | cut -d '=' -f2`
 ssh adminDevOps@$masterip -o "StrictHostKeyChecking no" exit
 ssh adminDevOps@$workerip -o "StrictHostKeyChecking no" exit
