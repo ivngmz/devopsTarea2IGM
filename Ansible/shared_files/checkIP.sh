bf3aa86f9c0d034c7fab198a3fcd164ee906f80f
ivngmz2018 <ivngmz2018@gmail.com>
2022-03-06 07:24:13 +0000

    Automatizo recuperacion de IP con az cli

--- Ansible/shared_files/checkIP.sh
+++ Ansible/shared_files/checkIP.sh
@@ -2,13 +2,13 @@
 az account set --subscription=b32052cd-407e-4571-ad05-c3a40dcec58c;
 
 
-masterip=$(az vm list-ip-addresses --resource-group KUBERNETES_RG --name master-vm | jq '.[0].virtualMachine.network.publicIpAddresses[0].ipAddress')
+masterip=$(az vm list-ip-addresses --resource-group KUBERNETES_RG --name master-vm | jq -r '.[0].virtualMachine.network.publicIpAddresses[0].ipAddress')
 
-workerip=$(az vm list-ip-addresses --resource-group KUBERNETES_RG --name worker1-vm | jq '.[0].virtualMachine.network.publicIpAddresses[0].ipAddress')
+worker1ip=$(az vm list-ip-addresses --resource-group KUBERNETES_RG --name worker1-vm | jq -r '.[0].virtualMachine.network.publicIpAddresses[0].ipAddress')
 
 
 # Modificar inventario
 
-sed -i "s/^MasterIP=.*/MasterIP=$masterip/g" test
-sed -i "s/^MasterIP=.*/Worker1=$workerip/g" test
+sed -i "s/^masterIP=.*/masterIP=$masterip/g" inventory 
+sed -i "s/^worker1IP=.*/worker1IP=$worker1ip/g" inventory
 
