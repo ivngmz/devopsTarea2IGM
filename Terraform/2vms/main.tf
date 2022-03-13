d97499590abcd714498c4757a7c7019f5524b2de
ivngmz2018 <ivngmz2018@gmail.com>
2022-03-13 09:56:30 +0100

    Update main.tf

--- Terraform/2vms/main.tf
+++ Terraform/2vms/main.tf
@@ -1,22 +1,4 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-#   terraform {
-#     required_providers {
-#       azurerm = {
-#         source  = "hashicorp/azurerm"
-#         version = "~>2.46.1"
-#       }
-#     }
-#   }
-#   
-#   # crea un service principal y rellena los siguientes datos para autenticar
-#   		!!! Creo provider.tf para estos datos y añado a git ignore ¡¡¡
-#   provider "azurerm" {
-#     features {}
-#         subscription_id = ""
-#         client_id = "" # appID
-#         client_secret = "" # password
-#         tenant_id = "" # tenant
-#   }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 
@@ -34,7 +16,7 @@ resource "azurerm_resource_group" "rg" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 
 resource "azurerm_storage_account" "stAccount" {
-    name                     = "staccountcpdounir" 
+    name                     = var.storage_account
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
     account_tier             = "Standard"
